82807b56ddfb09ce9d0f8fa216bcd34646685aed
Knut Jackowski <doxanthropos@googlemail.com>
2018-05-13 16:58:46 +0200

    commented the little JS script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* This little script changes the message at the top of the page 
+ * For now there are not many possible message, but that will hopefully change */
 const messages = ["Just Do It!","QUIDQUID AGIS PRUDENTER AGAS ET RESPICE FINEM"];
 window.onload = function(){
 let newMessage = getRandomMessage(messages);
