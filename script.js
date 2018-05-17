b66874fc2acfbb818b05a3cc317b2d837853791a
Knut Jackowski <doxanthropos@googlemail.com>
2018-05-17 22:18:51 +0200

    fixed indention

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 /* This little script changes the message at the top of the page 
  * For now there are not many possible message, but that will hopefully change */
-const messages = ["Just Do It!","QUIDQUID AGIS PRUDENTER AGAS ET RESPICE FINEM"];
+const messages = [
+  "Just Do It!",
+  "QUIDQUID AGIS PRUDENTER AGAS ET RESPICE FINEM"
+  ];
 window.onload = function(){
-let newMessage = getRandomMessage(messages);
-let message = document.getElementById("message");
-message.innerHTML = newMessage;  	
+  let newMessage = getRandomMessage(messages);
+  let message = document.getElementById("message");
+  message.innerHTML = newMessage;  	
 }
 
 const getRandomMessage = function(messageList){
